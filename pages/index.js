7121a55b0ed7d665c2286b4e1da0960622c81271
bcaravana <bcaravana@maximgrp.com>
2023-03-02 20:06:49 -0500

    Sign in with spotify using next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,41 @@
 import { BsSpotify } from "react-icons/bs";
-export default function Login() {
-  const handleLoginClick = async () => {
-    await fetch("/api/login");
-  };
+import { useSession, signIn, signOut } from "next-auth/react";
 
-  return (
-    <div className="flex flex-col h-full justify-center items-center bg-zinc-900">
-      <div className="flex items-center justify-center gap-3 text-white mb-3">
-        <span className="text-[#1DB954]">
-          <BsSpotify size={40} />
-        </span>
-        <h1 className="text-4xl">Spotify Analytics</h1>
+export default function Login() {
+  const { data: session } = useSession();
+  console.log(session);
+
+  if (session) {
+    return (
+      <div>
+        <h1>Signed in</h1>
+
+        <h2>Signout</h2>
+
+        <button
+          onClick={signOut}
+          className="text-white p-2 rounded-lg border border-[#1DB954] border-solid hover:border-zinc-500 hover:text-zinc-500 transition-all"
+        >
+          Logout
+        </button>
       </div>
-      <button
-        onClick={handleLoginClick}
-        className="text-white p-2 rounded-lg border border-[#1DB954] border-solid hover:border-zinc-500 hover:text-zinc-500 transition-all"
-      >
-        Login with Spotify
-      </button>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="flex flex-col h-full justify-center items-center bg-zinc-900">
+        <div className="flex items-center justify-center gap-3 text-white mb-3">
+          <span className="text-[#1DB954]">
+            <BsSpotify size={40} />
+          </span>
+          <h1 className="text-4xl">Spotify Analytics</h1>
+        </div>
+        <button
+          onClick={signIn}
+          className="text-white p-2 rounded-lg border border-[#1DB954] border-solid hover:border-zinc-500 hover:text-zinc-500 transition-all"
+        >
+          Login with Spotify
+        </button>
+      </div>
+    );
+  }
 }
